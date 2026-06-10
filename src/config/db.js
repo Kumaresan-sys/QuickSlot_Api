30c0b23a1cd5cd2b8984854c418b6c625a5685eb
Kumaresan-sys <kumaresan64761@gmail.com>
2026-06-10 23:43:59 +0530

    fixed db config issues

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,21 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+const poolConfig = process.env.DB_URL 
+  ? { 
+      connectionString: process.env.DB_URL,
+      ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+    }
+  : {
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    };
+
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  ...poolConfig,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
